8bc2b079960a1133ec60ae6b93fa1bafd44b916d
Enrico Signoretti <esignoretti@MBA-ES-5.local>
2026-05-29 20:01:30 +0200

    fix: remove explicit DELIM/QUOTE from log/syslog read_csv, let AUTO_DETECT handle it

--- internal/convert/engine.go
+++ internal/convert/engine.go
@@ -197,17 +197,11 @@ func (e *Engine) convertFile(file, bucket, endpoint, accessKey, secretKey string
 	var readSQL string
 	switch f {
 	case "syslog":
-		readSQL = fmt.Sprintf(`
-			SELECT *
-			FROM read_csv('%s', AUTO_DETECT=TRUE, DELIM=' ', QUOTE='"', HEADER=FALSE)
-		`, s3Path)
+		readSQL = fmt.Sprintf(`SELECT * FROM read_csv('%s', AUTO_DETECT=TRUE, HEADER=FALSE)`, s3Path)
 	case "json":
 		readSQL = fmt.Sprintf("SELECT * FROM read_json_auto('%s')", s3Path)
 	case "log":
-		readSQL = fmt.Sprintf(`
-			SELECT *
-			FROM read_csv('%s', AUTO_DETECT=TRUE, DELIM=' ', QUOTE='"', HEADER=FALSE)
-		`, s3Path)
+		readSQL = fmt.Sprintf(`SELECT * FROM read_csv('%s', AUTO_DETECT=TRUE, HEADER=FALSE)`, s3Path)
 	default:
 		readSQL = fmt.Sprintf("SELECT * FROM read_csv_auto('%s', HEADER=FALSE)", s3Path)
 	}
